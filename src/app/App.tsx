30d7d332734ee4d4cadf958d24834557cecd27eb
Alan Rad <alan.rad@emsbk.com>
2020-02-03 00:06:35 +1100

    feat: state provider and routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,31 @@
 import React, { FC, useReducer } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+// store
 import { AppProvider } from 'app/contexts';
+// reducer
 import { surveyResultsReducer, surveysInitialState } from 'app/reducers';
+// containers
+import { SurveysList, SurveyDetails } from 'app/containers';
+// components
+import { ContentWrapper } from 'app/components';
+// styles
+import 'styles/app.css';
 
-const Page1: FC = () => (
-  <>
-    <div className="page-1-wrapper">
-      <p>Page One</p>
-    </div>
-  </>
-);
-
-const Page2: FC = () => (
-  <>
-    <div className="page-1-wrapper">
-      <p>Page Two</p>
-    </div>
-  </>
-);
-
-export const App: FC = () => (
-  <AppProvider value={useReducer(surveyResultsReducer, surveysInitialState)}>
-    <p>Front End Coding Test</p>
-    <Router>
-      <Route default path={['/', '/page1']} component={Page1} />
-      <Route path={['/page2']} component={Page2} />
-    </Router>
-  </AppProvider>
-);
+export const App: FC = () => {
+  const [state, dispatch] = useReducer(
+    surveyResultsReducer,
+    surveysInitialState
+  );
+  const value = { state, dispatch };
+  return (
+    <AppProvider value={value}>
+      <ContentWrapper appName="Front End Coding Test">
+        <Router>
+          <Redirect exact from="/" to="/surveys-results" />
+          <Route default path={['/surveys-results']} component={SurveysList} />
+          <Route path={['/surveys/:id']} component={SurveyDetails} />
+        </Router>
+      </ContentWrapper>
+    </AppProvider>
+  );
+};
