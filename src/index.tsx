af8d1c3c3ad9498d1c16882f047c084586f3561e
Alan Rad <alan.rad@emsbk.com>
2020-02-02 13:24:03 +1100

    refactor: check for app config availability

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './app/App';
+import { Failed } from './app/Failed';
 import * as serviceWorker from './serviceWorker';
+// declare global configuration which the app needed
+declare global {
+  interface Window {
+    ROOT_URL: string;
+    SURVEY_PATH: string;
+  }
+}
+// make sure the required global config items are available
+const { ROOT_URL, SURVEY_PATH } = window;
+const isConfigAvailable =
+  typeof ROOT_URL !== 'undefined' &&
+  typeof SURVEY_PATH !== 'undefined' &&
+  !!ROOT_URL &&
+  !!SURVEY_PATH;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  isConfigAvailable ? <App /> : <Failed />,
+  document.getElementById('root')
+);
 
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
